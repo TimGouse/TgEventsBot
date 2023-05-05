7cf717e07ffc0d5935ba31e54b720dacd7a3de84
timgouse21 <108076525+TimGouse@users.noreply.github.com>
2023-05-05 21:46:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace TgEventsBot
         static async Task Main(string[] args)
         {
             
-            string telegramApiKey = "6055948934:AAEW4SxMgq0gdtyGps1BZL3wLxY9zJZAHm0" ;
-            string timePadApiKey = "ca822d0bacf22ed61c80e1e3363d91e4e8383294";
-            string databaseConnectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=1234";
+            string telegramApiKey = "" ;
+            string timePadApiKey = "";
+            string databaseConnectionString = "";
 
             var eventBot = new EventBot(telegramApiKey, timePadApiKey, databaseConnectionString);
             await eventBot.InitializeBot(telegramApiKey);
